c098ce8e2f766cf68df849a7970bdd99dbd36614
delma60 <oladeleofficial@gmail.com>
2026-06-28 02:35:13 +0100

    feat(api): integrate js-cookie for XSRF token handling in API requests

--- src/shared/api/apiClient.ts
+++ src/shared/api/apiClient.ts
@@ -1,16 +1,29 @@
 import axios from 'axios';
 import { env } from '../env';
+import Cookies from "js-cookie";
 
 export const apiClient = axios.create({
   // Replace with your actual backend API base URL
   baseURL: env("VITE_API_BASE_URL", 'https://api.swiftvtu.com/api/v1'),
-  timeout: 50000,
   withCredentials: true,
+  withXSRFToken: true,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
   },
 });
+
+apiClient.interceptors.request.use((config) => {
+  if (config.url?.includes("/sanctum/csrf-cookie")) {
+    config.baseURL = import.meta.env.VITE_BACKEND; // no /api
+  }
+  const token = Cookies.get("XSRF-TOKEN");
+  if (token) {
+    config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
+  }
+  return config;
+});
+
 
 // Request Interceptor: Automatically attach the auth token to outgoing requests
 apiClient.interceptors.request.use(
